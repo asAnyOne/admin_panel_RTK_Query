ee74bdc1dbe7901a8c8eb7f511c9b024a0557109
ODILBEK <ooruzmetov@.gmail.com>
2022-09-09 12:45:55 +0500

    js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./heroes.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
-const path = require("path");
-const express = require("express");
-
+import { create, router as _router, defaults } from "json-server";
+import { join } from "path";
+import { static as _static } from "express";
+const server = create();
+const router = _router("heroes.json");
+const middlewares = defaults({ static: "build" });
 const port = process.env.PORT || 3001;
 
 // server.use(middlewares);
 // server.use(jsonServer.rewriter({ "api/*": "/$1" }));
 // server.use(router);
 server.use("/heroes", middlewares, router);
-server.use(express.static(path.join(__dirname, "build")));
+server.use(_static(join(__dirname, "build")));
 
 server.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(join(__dirname, "build", "index.html"));
 });
 
 server.listen(port);
